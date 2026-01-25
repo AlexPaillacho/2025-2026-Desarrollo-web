da1df31d850d2cc04770abaa6ee39572172a9d74
AlexPaillacho <ad.natop@uea.edu.ec>
2026-01-25 07:55:04 -0500

    cambios

--- Semana 7/Script.js
+++ Semana 7/Script.js
@@ -1,11 +1,10 @@
 const products= [
 
-    {id 1, nombre:"", precio: 20},
-    {id 2,},
-    {id 3,},
-    {id 4,},
-    {id 5,},
-    {id 6,},
+    {id: 1 , nombre:"", precio: 20, descripcion:""},
+    {id: 1, nombre:"", precio: 20, descripcion:""},
+    {id: 1, nombre:"", precio: 20, descripcion:""},
+    {id: 1, nombre:"", precio: 20, descripcion:""},
+    {id: 1, nombre:"", precio: 20, descripcion:""},
+    
 
-
-]
+];
